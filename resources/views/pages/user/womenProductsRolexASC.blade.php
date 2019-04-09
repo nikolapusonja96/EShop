b2f7446d4fcb2b3c71da5d6ed15cb764fecca77f
Nikola Pusonja <49130403+nikolapusonja96@users.noreply.github.com>
2019-04-09 02:41:58 +0200

    Delete womenProductsRolexASC.blade.php

--- resources/views/pages/user/womenProductsRolexASC.blade.php
+++
@@ -1,7 +0,0 @@
-@extends('layouts.frontLayout')
-@section('title')
-    <title>E-SHOP | Women Rolex</title>
-@endsection
-@section('section_top')
-    @include('components.user.showWomenRolexASC')
-@endsection
