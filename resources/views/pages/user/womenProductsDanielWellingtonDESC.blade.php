e519c01c2dbce033e1a56e6d9a94ea75b5f26c9f
Nikola Pusonja <49130403+nikolapusonja96@users.noreply.github.com>
2019-04-09 02:42:37 +0200

    Delete womenProductsDanielWellingtonDESC.blade.php

--- resources/views/pages/user/womenProductsDanielWellingtonDESC.blade.php
+++
@@ -1,7 +0,0 @@
-@extends('layouts.frontLayout')
-@section('title')
-    <title>E-SHOP | DanielW</title>
-@endsection
-@section('section_top')
-    @include('components.user.showDanielWellingtonDESC')
-@endsection
