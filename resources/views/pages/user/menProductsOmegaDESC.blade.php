1174adab89ae73bb92bc1495f522be6e57e2d675
Nikola Pusonja <49130403+nikolapusonja96@users.noreply.github.com>
2019-04-09 02:31:48 +0200

    Delete menProductsOmegaDESC.blade.php

--- resources/views/pages/user/menProductsOmegaDESC.blade.php
+++
@@ -1,7 +0,0 @@
-@extends('layouts.frontLayout')
-@section('title')
-    <title>E-SHOP | Men Omega</title>
-@endsection
-@section('section_top')
-    @include('components.user.showMenOmegaDESC')
-@endsection
