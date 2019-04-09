1286dca61b160439ce8826dd90957f48206a809d
Nikola Pusonja <49130403+nikolapusonja96@users.noreply.github.com>
2019-04-09 02:31:17 +0200

    Delete menProductsUlysseNardinDESC.blade.php

--- resources/views/pages/user/menProductsUlysseNardinDESC.blade.php
+++
@@ -1,7 +0,0 @@
-@extends('layouts.frontLayout')
-@section('title')
-    <title>E-SHOP | Men Ulysse Nardin</title>
-@endsection
-@section('section_top')
-    @include('components.user.showUlysseNardinDESC')
-@endsection
