f2fc352bd17ec42e64d2d516b53304badaa97b3e
Nikola Pusonja <49130403+nikolapusonja96@users.noreply.github.com>
2019-04-09 02:42:12 +0200

    Delete womenProductsOmegaDESC.blade.php

--- resources/views/pages/user/womenProductsOmegaDESC.blade.php
+++
@@ -1,7 +0,0 @@
-@extends('layouts.frontLayout')
-@section('title')
-    <title>E-SHOP | Women Omega</title>
-@endsection
-@section('section_top')
-    @include('components.user.showWomenProductOmegaDESC')
-@endsection
